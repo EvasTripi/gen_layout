f810c2f7e6ebacf0effe0071fcf1f22ea1ee6d06
Arturo Morales S <a.morales.stats@gmail.com>
2016-02-24 23:10:21 -0600

    Modificando el Setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email='a.morales.stats@gmail.com',
     version='1.0',
     url='http://github.com/EvasTripi/gen_layout',
-    packages=['gen_layout'],
+    scripts=['gen_layout/gen_layout'],
     description='Programa para generar layouts de CsPro a Excel',
     classifiers=[
         'Programming Language :: Python',
